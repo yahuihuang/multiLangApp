44db9c62bc141c78e4f1e9a428a5296570838692
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-21 15:56:38 +0800

    Initial Commit

---
+++ multiLangApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiLangApp
+//
+//  Created by grace on 2019/12/21.
+//  Copyright © 2019 grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
